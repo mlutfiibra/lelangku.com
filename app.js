9cc53071bea40506f362743564f1dcbfc41f4c3a
lutfi <mlutfiibra@outlook.com>
2019-03-28 13:30:19 +0700

    Implement CRUD in user model

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const app = express()
 const bodyParser = require('body-parser');
 const PORT = 5400
 const routes = require('./routes')
-const items = require('/routes/items')
-const users = require('/routes/users')
+// const items = require('/routes/items')
+const users = require('./routes/users')
+const multer  = require('multer')
+const upload = multer({ dest: 'public/img' })
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('view engine', 'ejs')
+app.set('view engine', 'ejs')
 
-app.use('/items', items)
-app.use('/users', users)
+// app.use('/items', items)
+app.use('/users', upload.single('img_path'), users)
 app.use('/', routes)
 
 app.listen(PORT, ()=> console.log(`Listening to radio ${PORT} F.M`))
--- models/user.js
+++ models/user.js
@@ -1,18 +1,68 @@
 'use strict';
+
+const bcrypt = require('bcryptjs');
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op;
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
-    username: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Format must an email'
+        }
+      }
+    },
     password: DataTypes.STRING,
     img_path: DataTypes.STRING,
     location: DataTypes.STRING,
     address: DataTypes.STRING,
     balance: DataTypes.INTEGER,
     role: DataTypes.STRING,
+    salt: DataTypes.STRING,
     isLoggedIn: DataTypes.INTEGER
-  }, {});
+  }, {
+    hooks : {
+      afterBulkCreate(user, options) {
+        this.generateSaltAndPassword(user)
+      },
+      beforeCreate(user, option) {
+        this.generateSaltAndPassword(user)
+      },
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  User.generateSaltAndPassword = function(user) {
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(user.dataValues.password, salt);
+    user.dataValues.salt = salt
+    user.dataValues.password = hash
+  }
+
+  User.prototype.comparePasswordWithSalt = function(password, user) {
+    return bcrypt.compareSync(password, user.password);
+  }
+
+  User.isEmailUnique = function (input){
+    return User.findOne({
+      where: {
+        email: input
+      }
+    })
+    .then(user => {
+      if(user) {
+        return true
+      }else {
+        return false        
+      }
+    })
+  }
+
   return User;
 };
