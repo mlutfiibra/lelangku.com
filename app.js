1fd8512ad369b550c1a13cc3893d57a2f85459ea
qoyyima22 <qoyyimafiassalam@gmail.com>
2019-03-28 13:33:20 +0700

    items crud

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const app = express()
 const bodyParser = require('body-parser');
 const PORT = 5400
 const routes = require('./routes')
-const items = require('/routes/items')
-const users = require('/routes/users')
+const items = require('./routes/items')
+const users = require('./routes/users')
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('view engine', 'ejs')
+app.set('view engine', 'ejs')
 
 app.use('/items', items)
-app.use('/users', users)
+// app.use('/users', users)
 app.use('/', routes)
 
-app.listen(PORT, ()=> console.log(`Listening to radio ${PORT} F.M`))
+app.listen(PORT, ()=> console.log(`Listening to ${new Date} radio ${PORT} F.M`))
+
